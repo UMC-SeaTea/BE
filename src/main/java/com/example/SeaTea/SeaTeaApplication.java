48d65ca30946418e7c920ac285bf91d4e2fc15b7
lsw71311 <lsw71311@naver.com>
2026-01-04 10:05:37 +0900

    Init commit

---
+++ src/main/java/com/example/SeaTea/SeaTeaApplication.java
@@ -0,0 +1,13 @@
+package com.example.SeaTea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaTeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeaTeaApplication.class, args);
+	}
+
+}
