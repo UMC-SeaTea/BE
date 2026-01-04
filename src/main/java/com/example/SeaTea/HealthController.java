e29209d5937886d34640f9f00c29538af9f3f76c
lsw71311 <lsw71311@naver.com>
2026-01-04 11:17:46 +0900

    [SETTING] 서버 헬스체크 controller

---
+++ src/main/java/com/example/SeaTea/HealthController.java
@@ -0,0 +1,19 @@
+package com.example.SeaTea;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+@Tag(name = "서버 안정성 체크 API")
+public class HealthController {
+
+    @GetMapping
+    @Operation(summary = "헬스 체크 API")
+    public String health() {
+        return "healthy!";
+    }
+}
