7f07a3c6160e8950946de4d91d9c13f2f33bf624
서재민 <dean8638@naver.com>
2026-01-14 00:09:17 +0900

    [FEAT] 로그인 Member 엔티티 추가

---
+++ src/main/java/com/example/SeaTea/domain/member/entitiy/Member.java
@@ -0,0 +1,36 @@
+package com.example.SeaTea.domain.member.entitiy;
+
+import com.example.SeaTea.global.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "member")
+public class Member extends BaseEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+  @Column(name = "memberId")
+  private Long id;
+
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(nullable = false, unique = true)
+  private String nickname;
+
+//  role 인가처리
+//  @Enumerated(EnumType.STRING)
+//  private Role role; // USER, ADMIN
+
+//  status 탈퇴/차단 처리
+//  @Enumerated(EnumType.STRING)
+//  private MemberStatus status; // ACTIVE, BLOCKED, DELETED
+}
