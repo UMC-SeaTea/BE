ec29c04dd5a62ce30d844aa8398e49a419a525e5
trymimi <applemimicom@gmail.com>
2026-01-15 12:34:50 +0900

    [Feat] 장소, 티백 엔티티 추가

---
+++ src/main/java/com/example/SeaTea/domain/place/entity/MemberSavedPlace.java
@@ -0,0 +1,44 @@
+package com.example.SeaTea.domain.place.entity;
+
+import com.example.SeaTea.domain.member.entitiy.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "member_saved_space")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class MemberSavedPlace {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_saved_space_id")
+    private Long memberSavedSpaceId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "place_id")
+    private Place place;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/com/example/SeaTea/domain/place/entity/Place.java
@@ -0,0 +1,60 @@
+package com.example.SeaTea.domain.place.entity;
+
+import com.example.SeaTea.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "place")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Place extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "place_id")
+    private Long placeId;
+
+    // TastingType 엔티티 생성 후 FK 관계 매핑
+    // @ManyToOne(fetch = FetchType.LAZY)
+    // @JoinColumn(name = "tasting_type_id")
+    // private TastingType tastingType;
+    @Column(name = "tasting_type_id")
+    private Long tastingTypeId;
+
+    @Column(name = "naver_place_id", unique = true)
+    private Long naverPlaceId;
+
+    @Column(name = "neme")
+    private String neme;
+
+    @Column(name = "thumbnail_image_url", length = 255)
+    private String thumbnailImageUrl;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "road_address")
+    private String roadAddress;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "lng", precision = 11, scale = 7)
+    private BigDecimal lng;
+
+    @Column(name = "lat", precision = 11, scale = 7)
+    private BigDecimal lat;
+
+    @Column(name = "opening_hours", columnDefinition = "TEXT")
+    private String openingHours;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "note", columnDefinition = "TEXT")
+    private String note;
+}
