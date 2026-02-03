6a3dd1683879911f801e4656dc0cdb8be6181af0
서재민 <dean8638@naver.com>
2026-02-03 15:03:34 +0900

    로그인 및 회원가입

--- src/main/java/com/example/SeaTea/domain/member/service/command/MemberCommandServiceImpl.java
+++ src/main/java/com/example/SeaTea/domain/member/service/command/MemberCommandServiceImpl.java
@@ -6,10 +6,10 @@ import com.example.SeaTea.domain.member.dto.response.MemberResDTO;
 import com.example.SeaTea.domain.member.entity.Member;
 import com.example.SeaTea.domain.member.repository.MemberRepository;
 import com.example.SeaTea.global.auth.enums.Role;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,7 +24,7 @@ public class MemberCommandServiceImpl implements MemberCommandService {
   public MemberResDTO.JoinDTO signup(
       MemberReqDTO.JoinDTO dto
   ){
-    // 솔트된 비밀번호 생성
+    // 솔트된 비밀번호 생성(salt+hash가 합쳐진 비밀번호 문자열)
     String salt = passwordEncoder.encode(dto.password());
 
     // 사용자 생성
