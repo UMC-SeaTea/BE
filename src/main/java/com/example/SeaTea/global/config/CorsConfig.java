816981cbe8752003c9489d61a62524a740e2f20d
lsw71311 <lsw71311@naver.com>
2026-02-07 00:57:52 +0900

    [SETTING] CofsConfig 추가

---
+++ src/main/java/com/example/SeaTea/global/config/CorsConfig.java
@@ -0,0 +1,33 @@
+package com.example.SeaTea.global.config;
+
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+
+    // 허용 오리진 목록
+    configuration.setAllowedOrigins(
+        List.of("http://localhost:5173", "https://seatea-site.vercel.app"));
+    configuration.addAllowedHeader("*");
+    configuration.setExposedHeaders(List.of("access", "Authorization"));
+    configuration.setAllowedMethods(
+        List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+
+    // 자격증명 허용
+    configuration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
+
+}
